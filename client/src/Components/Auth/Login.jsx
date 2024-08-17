32ed8ec9595d0693af41745a3e0059879535a5a2
Mohit Kumar <80189839+iammk31@users.noreply.github.com>
2024-08-17 16:41:42 +0530

    signup and login ui changes

--- client/src/Components/Auth/Login.jsx
+++ client/src/Components/Auth/Login.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
 import './Login.css';
 import {
   MDBContainer,
@@ -10,9 +14,36 @@ import {
   MDBIcon,
   MDBInput
 }
-from 'mdb-react-ui-kit';
+  from 'mdb-react-ui-kit';
 
-const Register = () => {
+const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:4000/api/v1/cgate/login",
+        { email, password },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+      toast.success(data.message);
+      setEmail("");
+      setPassword("");
+      navigate("/success");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
   return (
     <MDBContainer className="register-container">
 
@@ -20,25 +51,38 @@ const Register = () => {
         <MDBRow className='g-0'>
 
           <MDBCol md='6'>
-            <img src='../../images/LoginImage.png' alt="login form" style={{ width: '500px', height: '650px' }}/>
+            <img src='../../images/LoginImage.png' alt="login form" style={{ width: '500px', height: '650px' }} />
           </MDBCol>
 
           <MDBCol md='6'>
             <MDBCardBody className='d-flex flex-column'>
 
               <div className='d-flex flex-row mt-2'>
-                <MDBIcon fas icon="cubes fa-3x me-3" style={{ color: '#ff6219' }}/>
+                <MDBIcon fas icon="cubes fa-3x me-3" style={{ color: '#ff6219' }} />
                 <img src={'../../images/logo2.gif'} alt="" style={{ width: '68px', height: '55px' }} />
               </div>
 
-              <h5 className="fw-normal my-4 pb-3" style={{letterSpacing: '1px'}}>Sign into your account</h5>
+              <h5 className="fw-normal my-4 pb-3" style={{ letterSpacing: '1px' }}>Login into your account</h5>
 
-                <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg"/>
-                <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg"/>
+              <MDBInput wrapperClass='mb-4'
+                label='Email address'
+                id='formControlLg'
+                type='email'
+                size="lg"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)} />
+              <MDBInput wrapperClass='mb-4'
+                label='Password'
+                id='formControlLg'
+                type='password'
+                size="lg"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)} />
 
-              <button className="button-Login">Login</button>
-              <a className="small text-muted" href="#!">Forgot password?</a>
-              <p className="mb-5 pb-lg-2" style={{color: '#393f81'}}>Don't have an account? <a href="#!" style={{color: '#393f81'}}><Link to={"/Signup"}>Register here</Link></a></p>
+              <button className="button-Login"
+                type="submit"
+                onClick={handleLogin}>Login</button>
+              <p className="mb-5 pb-lg-2" style={{ color: '#393f81' }}>Don't have an account? <a href="#!" style={{ color: '#393f81' }}><Link to={"/Signup"}>Register here</Link></a></p>
 
               <div className='d-flex flex-row justify-content-start'>
                 <a href="#!" className="small text-muted me-1">Terms of use.</a>
@@ -55,4 +99,4 @@ const Register = () => {
   );
 }
 
-export default Register;
+export default Login;
--- client/src/Components/Auth/Signup.jsx
+++ client/src/Components/Auth/Signup.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './Signup.css';
-import { HiOutlineArrowNarrowRight } from "react-icons/hi";
+// import { HiOutlineArrowNarrowRight } from "react-icons/hi";
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import axios from "axios";
+import {
+    MDBContainer,
+    MDBCard,
+    MDBCardBody,
+    MDBRow,
+    MDBCol,
+    MDBIcon,
+    MDBInput
+}
+    from 'mdb-react-ui-kit';
+
 
 
 
@@ -13,8 +25,7 @@ const Signup = () => {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
-    const [date, setDate] = useState("");
-    const [time, setTime] = useState("");
+    const [password, setPassword] = useState("");
     const [phone, setPhone] = useState("");
     const navigate = useNavigate();
 
@@ -23,7 +34,7 @@ const Signup = () => {
         try {
             const { data } = await axios.post(
                 "http://localhost:4000/api/v1/cgate/send",
-                { firstName, lastName, email, phone, date, time },
+                { firstName, lastName, email, phone, password },
                 {
                     headers: {
                         "Content-Type": "application/json",
@@ -36,8 +47,7 @@ const Signup = () => {
             setLastName("");
             setPhone("");
             setEmail("");
-            setTime("");
-            setDate("");
+            setPassword("");
             navigate("/success");
         } catch (error) {
             toast.error(error.response.data.message);
@@ -45,63 +55,78 @@ const Signup = () => {
     };
     return (
 
-        <div className="banner">
-            <div className="reservation_form_box">
-                <h1>SignUp</h1>
-                <p>For Further Questions, Please Call</p>
-                <form>
-                    <div>
-                        <input
-                            type="text"
-                            placeholder="First Name"
-                            value={firstName}
-                            onChange={(e) => setFirstName(e.target.value)}
-                        />
-                        <input
-                            type="text"
-                            placeholder="Last Name"
-                            value={lastName}
-                            onChange={(e) => setLastName(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <input
-                            type="date"
-                            placeholder="Date"
-                            value={date}
-                            onChange={(e) => setDate(e.target.value)}
-                        />
-                        <input
-                            type="time"
-                            placeholder="Time"
-                            value={time}
-                            onChange={(e) => setTime(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <input
-                            type="email"
-                            placeholder="Email"
-                            className="email_tag"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                        <input
-                            type="number"
-                            placeholder="Phone"
-                            value={phone}
-                            onChange={(e) => setPhone(e.target.value)}
-                        />
-                    </div>
-                    <button type="submit" onClick={handleSignup}>
-                        SignUp{" "}
-                        <span>
-                            <HiOutlineArrowNarrowRight />
-                        </span>
-                    </button>
-                </form>
-            </div>
-        </div>
+        <MDBContainer className="register-container">
+
+            <MDBCard>
+                <MDBRow className='g-0'>
+
+                    <MDBCol md='6'>
+                        <img src='../../images/LoginImage.png' alt="login form" style={{ width: '500px', height: '650px' }} />
+                    </MDBCol>
+
+                    <MDBCol md='6'>
+                        <MDBCardBody className='d-flex flex-column'>
+
+                            <div className='d-flex flex-row mt-2'>
+                                <MDBIcon fas icon="cubes fa-3x me-3" style={{ color: '#ff6219' }} />
+                                <img src={'../../images/logo2.gif'} alt="" style={{ width: '68px', height: '55px' }} />
+                            </div>
+
+                            <h5 className="fw-normal my-4 pb-3" style={{ letterSpacing: '1px' }}>SignUp into your account</h5>
+
+                            <MDBInput wrapperClass='mb-4'
+                                label='FirstName'
+                                id='formControlLg'
+                                type='text'
+                                size="lg"
+                                value={firstName}
+                                onChange={(e) => setFirstName(e.target.value)} />
+                            <MDBInput wrapperClass='mb-4'
+                                label='LastName'
+                                id='formControlLg'
+                                type='text'
+                                size="lg"
+                                value={lastName}
+                                onChange={(e) => setLastName(e.target.value)} />
+
+                            <MDBInput wrapperClass='mb-4'
+                                label='Email address'
+                                id='formControlLg'
+                                type='email'
+                                size="lg"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)} />
+                            <MDBInput wrapperClass='mb-4'
+                                label='Phone'
+                                id='formControlLg'
+                                type='number'
+                                size="lg"
+                                value={phone}
+                                onChange={(e) => setPhone(e.target.value)} />
+                            <MDBInput wrapperClass='mb-4'
+                                label='Password'
+                                id='formControlLg'
+                                type='password'
+                                size="lg"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)} />
+
+                            <button className="button-Login"
+                                type="submit"
+                                onClick={handleSignup}>SignUp</button>
+
+                            <div className='d-flex flex-row justify-content-start'>
+                                <a href="#!" className="small text-muted me-1">Terms of use.</a>
+                                <a href="#!" className="small text-muted">Privacy policy</a>
+                            </div>
+
+                        </MDBCardBody>
+                    </MDBCol>
+
+                </MDBRow>
+            </MDBCard>
+
+        </MDBContainer>
 
 
     );
