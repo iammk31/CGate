df06efa0ad82a06325735957ce0fdfee774a0049
ankuraaryan5 <ankuraaryan5@gmail.com>
2024-08-18 14:19:38 +0530

    added a dropdown on hover to gate2025

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -32,7 +32,14 @@ const Navbar = () => {
                 <div className={styles.home}>
                     <li className={styles.item}><Link to={"/"}>Home</Link></li>
                     <li className={styles.item}><Link to={"/courses"}>Courses</Link></li>
-                    <li className={styles.item}><Link to={"/gate-special"}>Gate2025</Link></li>
+                    <li className={`${styles.item} ${styles.dropdown}`}>
+                        <Link to={"/gate-special"}>Gate2025</Link>
+                        <ul className={styles.dropdownMenu}>
+                            <li className={styles.dropdownItem}><Link to={"/gate-overview"}>Overview</Link></li>
+                            <li className={styles.dropdownItem}><Link to={"/gate-schedule"}>Schedule</Link></li>
+                            <li className={styles.dropdownItem}><Link to={"/gate-resources"}>Resources</Link></li>
+                        </ul>
+                    </li>
                 </div>
                 <div className={styles.login}>
                     <button className={styles.button}><Link to={"/Login"}>Login</Link></button>
